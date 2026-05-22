e8497a25b6ad7869f25e027426fc8fc8440f356f
Inotay Krisztián <inotay.krisztian@alerant.hu>
2026-05-22 11:29:17 +0000

    fixes

--- scripts/install-argocd.sh
+++ scripts/install-argocd.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 kubectl create namespace argocd --dry-run=client -o yaml | kubectl apply -f -
-kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl apply --server-side -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
 kubectl -n argocd rollout status deployment/argocd-server --timeout=300s
 kubectl -n argocd rollout status deployment/argocd-repo-server --timeout=300s
